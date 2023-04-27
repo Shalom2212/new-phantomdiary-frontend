fe532bf483911758cd8a9ac15e92a4911e21ebf7
Shalom2212 <shalom222002@gmail.com>
2023-04-27 17:34:31 +0530

    removal of NODE_ENV

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+// import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if(process.env.NODE_ENV === 'production') disableReactDevTools
+// if(process.env.NODE_ENV === 'production') disableReactDevTools
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <BrowserRouter>

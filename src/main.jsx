ac926c638396906110a146d2c4843b8b3f659f96
Shalom2212 <shalom222002@gmail.com>
2023-04-29 18:21:54 +0530

    fixed bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter,Routes,Route,Switch} from 'react-router-dom';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 // import { disableReactDevTools } from '@fvilers/disable-react-devtools';
@@ -9,11 +9,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <BrowserRouter>
-    <Switch>
       <Routes>
         <Route path="/*" element={<App/>}/>
       </Routes>
-    </Switch>
 </BrowserRouter>
 
 );
